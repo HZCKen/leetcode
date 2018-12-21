775464000729145e954ed54df35ce75659a882ef
huangzhicheng <13826457912@163.com>
2018-12-21 10:51:49 +0800

    189旋转数组

--- leetcode/leetcode/ViewController.swift
+++ leetcode/leetcode/ViewController.swift
@@ -12,8 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        let temp = RotateArray()
+        var a  = [1,2,3,4,5,6,7]
+        temp.rotate(&a, 3)
+//         RotateArray().rotate(&[1,2,3,4,5,6,7], 3)
     }
 
     override func didReceiveMemoryWarning() {
